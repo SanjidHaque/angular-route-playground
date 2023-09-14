8fdfdadcc2af10c222fe30150b673f1dcfe869fa
DESKTOP-6K6863I\Sk. Sanjidul Haque <sanjidulhaque@gmail.com>
2023-09-14 22:19:21 +0600

    Add enable tracing and use hash option in for root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true, useHash: true })],
   exports: [RouterModule],
   providers: [provideRouter(routes,
     withRouterConfig({
